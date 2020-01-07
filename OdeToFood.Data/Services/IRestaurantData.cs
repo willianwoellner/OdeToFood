be449121472dc2d588ff989368af21fa69a39688
Willian Bernatzki Woellner <willian.bernatzki@gmail.com>
2020-01-07 12:37:26 +0800

    Segundo commit

---
+++ OdeToFood.Data/Services/IRestaurantData.cs
@@ -0,0 +1,12 @@
+﻿using OdeToFood.Data.Models;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OdeToFood.Data.Services
+{
+    public interface IRestaurantData
+    {
+        IEnumerable<Restaurant> GetAll();
+    }
+
+}
